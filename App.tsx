98feb7f88d8a5a705b4f561c3b1d2cd805c829fe
Corentyn <corentyn.masset@gmail.com>
2022-02-02 10:40:54 +0100

    My first Commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Button} from 'react-native-paper';
 
 export default function App() {
   return (
+    <>
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Welcome to DoggoApp ! 🐶</Text>
       <StatusBar style="auto" />
     </View>
+
+    <View style={styles.title}>
+      <Text style={styles.textBeforeButton}>If you want to see a Doggo, click here !✨</Text>
+      <Button style={styles.buttonDoggo}><Text style={styles.textButton}>Doggo</Text></Button>
+    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+   
+    backgroundColor: '#8ef6e4',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop:75,
+    height:"25%"
+  },
+  title: {
+    flex:1,
+    alignItems: 'center',
+    backgroundColor:"#8ef6e4"
+    
+  },
+  buttonDoggo: {
+    marginTop:40,
+    backgroundColor: "#9896f1",
+    borderColor:"black"
+  },
+  textBeforeButton: {
+    fontSize:32,
+    fontWeight:"bold",
+    color: "#d59bf6"
+  },
+
+  textButton: {
+    color:"#fff",
+    fontWeight:"bold"
   },
 });
